727b1d1a4381bf06ca9247bea3e200bbefd045fd
Andy Mitilineos <Amitilineos6273@stu.d214.org>
2025-09-10 11:00:34 -0500

    Added headerview, the textfield and a button that adds the entered text to the list

---
+++ HeaderView.swift
@@ -0,0 +1,25 @@
+//
+//  HeaderView.swift
+//  GitHubPractice4
+//
+//  Created by Andreas Mitilineos on 9/10/25.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    @Binding var TaskOfType: Task
+    @State var EnteredTask: String = ""
+    var body: some View {
+        HStack{
+            TextField("Enter task", text: $EnteredTask)
+            Button{
+                EnteredTask=""
+            } label: {
+                Text("Add Task")
+            }
+        }
+        
+    }
+    
+}
